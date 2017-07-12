61ede51f6fc5c876fc1b30fe0a745a679eb85f87
Jared Sorge <jared@jsorge.net>
2017-07-12 12:09:26 -0700

    Added an extension to give a popped view controller

---
+++ TaphouseKit/TaphouseKit/Classes/Helpers/UINavigationController+Extensions.swift
@@ -0,0 +1,24 @@
+//
+//  UINavigationController+Extensions.swift
+//  TaphouseKit
+//
+//  Created by Jared Sorge on 7/12/17.
+//  Copyright © 2017 Taphouse Software. All rights reserved.
+//
+
+import UIKit
+
+public extension UINavigationController {
+    /// If the navigation controller just popped a view controller, this method returns it.
+    /// This is helpful when wanting to know what just happened when using a `UINavigationControllerDelegate`.
+    /// Got the idea from http://khanlou.com/2017/05/back-buttons-and-coordinators/
+    public func poppedViewController() -> UIViewController? {
+        guard
+            let fromViewController = transitionCoordinator?.viewController(forKey: .from),
+            viewControllers.contains(fromViewController) == false else {
+                return nil
+        }
+        
+        return fromViewController
+    }
+}
